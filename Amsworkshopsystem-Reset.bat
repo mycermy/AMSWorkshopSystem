ab86f7b2caf71fe29fa4690b2ad176ebd6c36d7b
mycermy <mycermy@gmail.com>
2023-10-06 15:16:52 +0800

    1st commit

---
+++ Amsworkshopsystem-Reset.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+echo Terminating Anydesk Process.
+for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
+for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
+for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
+cls
+echo Resetting AMSWorkshopSystem
