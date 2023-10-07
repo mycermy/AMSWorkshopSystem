4b791265858274c31b249926363cc6163b03caea
mycermy <mycermy@gmail.com>
2023-10-07 13:30:50 +0800

    add terminate script

--- Amsworkshopsystem-Reset.bat
+++ Amsworkshopsystem-Reset.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-echo Terminating Anydesk Process.
+echo Terminating AMSWorkshopSystem Process.
 for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
 for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
 for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
--- update.bat
+++ update.bat
@@ -1,4 +1,13 @@
 @echo off
+cls
+echo Terminating AMSWorkshopSystem Process.
+for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
+for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
+for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH') do echo %%A=%%B &tskill %%B>nul
+cls
+echo Updating AMSWorkshopSystem
+
+@echo off
 for /F %%i in (repo_paths.txt) do call :pull_repo %%i
 pause
 exit
