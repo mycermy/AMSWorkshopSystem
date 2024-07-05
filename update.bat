a05941615bb9d031b5986368a6bb1aba71388433
mycermy <mycermy@gmail.com>
2024-07-05 09:14:53 +0800

    Bing AI suggestion

--- update.bat
+++ update.bat
@@ -7,25 +7,43 @@ for /f "tokens=1,2 delims= " %%A in ('tasklist /FI "IMAGENAME eq javaw.exe" /NH'
 cls
 echo Updating AMSWorkshopSystem
 
-@echo off
-rem for /F %%i in (repo_paths.txt) do call :pull_repo %%i
-call :pull_repo
+:menu
+echo.
+echo 1. Pull Git Repository
+echo 2. Hard Reset Repository
+echo 3. Exit
+set /p choice="Enter your choice (1/2/3): "
 
-rem  call :reset_repo
+if "%choice%"=="1" (
+    call :pull_repo
+) else if "%choice%"=="2" (
+    call :reset_repo
+) else if "%choice%"=="3" (
+    exit
+) else (
+    echo Invalid choice. Please select 1, 2, or 3.
+    goto menu
+)
 
-pause
+:: Timer (1 minute delay)
+timeout /t 60 >nul
+
 exit
 
 :pull_repo
 echo Pulling %1
 cd %1
 git pull
-
+goto :eof
 
 :reset_repo
-echo Pulling %1
-cd %1
-git reset --hard
-
-
-@echo off
+set /p "password=Enter the password for hard reset: "
+if "%password%"=="hardreset" (
+    echo Resetting repo %1
+    cd %1
+    git reset --hard
+    git pull
+) else (
+    echo Incorrect password. Operation canceled.
+)
+goto :eof
